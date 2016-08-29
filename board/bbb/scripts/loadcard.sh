86daa4367f6410b07f339804d846b7c8f5d62b63
Joel Carlson <JoelsonCarl@gmail.com>
2016-08-29 10:01:19 -0500

    bbb: add mkcard.sh and loadcard.sh
    Adds mkcard.sh that can be used to format an SD card with appropriate
    partitions.
    Adds loadcard.sh that can be used to load a formatted SD card with the
    images produced from build.
    
    Signed-off-by: Joel Carlson <JoelsonCarl@gmail.com>

---
+++ board/bbb/scripts/loadcard.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+usage() {
+	echo "Usage: $0 <drive> <img_dir>"
+	exit 1;
+}
+
+fail() {
+	echo "$1"
+	exit 1;
+}
+
+if [ $# -ne 2 ]; then
+	usage
+fi
+
+DISK="$1"
+IMGDIR="$2"
+
+# Make sure SD card is not mounted
+umount "${DISK}*"
+
+# Copy boot files to boot partition
+mkdir mnt_boot || fail "Could not make mnt_boot"
+mount "${DISK}1" mnt_boot || fail "Could not mount mnt_boot"
+cp "${IMGDIR}/MLO" mnt_boot || fail "Could not copy U-Boot SPL"
+cp "${IMGDIR}/u-boot.img" mnt_boot || fail "Could not copy U-Boot"
+cp "${IMGDIR}/uEnv.txt" mnt_boot || fail "Could not copy uEnv.txt"
+cp "${IMGDIR}/zImage" mnt_boot || fail "Could not copy kernel"
+cp "${IMGDIR}/am335x-boneblack.dtb" mnt_boot || fail "Could not copy device tree"
+sync
+umount mnt_boot || fail "Failure unmounting mnt_boot"
+rm -fr mnt_boot
+
+# Copy squashfs to rootfs partition
+dd if="${IMGDIR}/rootfs.squashfs" of="${DISK}2" bs=32M || \
+	fail "Could not copy rootfs"
+sync
---
+++ board/bbb/scripts/mkcard.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+usage() {
+	echo "Usage: $0 <drive>"
+	exit 1;
+}
+
+if [ $# -ne 1 ]; then
+	usage
+fi
+
+DISK="$1"
+
+# Make sure SD card is not mounted
+umount "/dev/${DISK}*"
+
+# Erase SD card
+# Erasing a full card can be slow; only erase partially into last partition
+dd if=/dev/zero of="${DISK}" bs=32M count=400
+
+# AM335x can boot from SD via two methods:
+# 1. Raw Mode: booting image expected at one of four specific locations
+# 2. FAT Mode: 'MLO' file found in root directory on an active primary
+#              partition of type FAT 12/16 or FAT32
+#
+# We will be using FAT Mode
+
+# Assumes sfdisk >= 2.26.x
+# partition table type: dos
+# partition table identifier: FAT32 with LBA
+# 1st partition: 50MiB FAT32
+# 2nd partition: 256MiB Linux (any Linux file system; will hold squashfs)
+# 3rd partition: remaining space, Linux (writeable user partition)
+# 4th partition: none
+{
+	echo "label: dos"
+	echo "label-id: 0x0C"
+	echo "start=1MiB,size=50MiB,bootable,type=c"
+	echo "size=256MiB,type=83"
+	echo "type=83"
+	echo "write"
+} | sfdisk "${DISK}"
+
+# Format boot (FAT32) partition
+# FAT32 must have at least 65,527 clusters
+# At one sector per cluster and 512 bytes per sector, minimum 32 MiB
+mkfs.vfat -F 32 -s 1 -S 512 -n "boot" "${DISK}1"
+# Format user partition
+mkfs.ext4 -L userfs "${DISK}3"
+# For mkfs.ext4 >= 1.43:
+# Supposedly U-Boot can't currently handle metadata_csum and 64bit
+# ext4 options, so disable for now.
+#mkfs.ext4 -L userfs -O ^metadata_csum,^64bit "${DISK}3"
+
+sync
